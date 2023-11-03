958d922e296906cef436a809f75eb509fdf8f670
alberto8812 <cvelascosaavedra@gmail.com>
2023-11-03 15:53:58 -0500

    confgi firebase

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClienty= new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <QueryClientProvider  client={queryClienty}>
+         <RouterProvider router={ router } />
+         <ReactQueryDevtools/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
