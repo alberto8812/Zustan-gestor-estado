d503016b49f499633e1b8ff59a40c05f7e337636
alberto8812 <cvelascosaavedra@gmail.com>
2023-11-03 18:20:19 -0500

    dectool use

--- src/sotre/person/person.store.ts
+++ src/sotre/person/person.store.ts
@@ -1,6 +1,6 @@
 import { type StateCreator, create } from "zustand";//el type es para que no importe nada
 
-import { StateStorage, createJSONStorage, persist } from "zustand/middleware";
+import { StateStorage, createJSONStorage, devtools, persist } from "zustand/middleware";
 import { customSessionStorage } from "..";
 import { customFirebaseStorage } from "../storages/firebase-storage.storage";
 
@@ -17,18 +17,19 @@ interface Actions {
     setLastName:(value:string)=>void,
     
 }
-const storeAPi: StateCreator<PersonState & Actions> = ( set ) => ( {
+const storeAPi: StateCreator<PersonState & Actions,[["zustand/devtools", never]]> = ( set ) => ( {
 
     firstName: '',
     lastName: '',
   
-    setFirstName: ( value: string ) => set( ( { firstName: value } )),
-    setLastName: ( value: string ) => set( ( { lastName: value } ) ),
+    setFirstName: ( value: string ) => set( (state)=> ( { firstName: value } ),false,'setFirstName'),
+    setLastName: ( value: string ) => set( (state)=>( { lastName: value } ),false,'setLastName' ),
   
   } );
 
 
 export const usePersonStore = create<PersonState  & Actions>()(
+  devtools(
   persist   (
         storeAPi,
         {
@@ -37,4 +38,5 @@ export const usePersonStore = create<PersonState  & Actions>()(
         }
 
     )
+  )
   );//nombre del local staore que le quiero asignar 
